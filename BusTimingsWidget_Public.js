c567b37b3f01755791635101e8d1a991b6c1b8d4
LZX Corp <107389832+LZXCorp@users.noreply.github.com>
2024-01-26 02:17:09 +0800

    Jan 2024 Patch

--- BusTimingsWidget_Public.js
+++ BusTimingsWidget_Public.js
@@ -1,6 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: orange; icon-glyph: magic;
+// icon-color: green; icon-glyph: magic;
+
 // Bus Timings Widget, Made by LZX
 // Use this with the Scriptable Application.
 
@@ -10,16 +11,11 @@
 
 let param = args.widgetParameter;
 
-let paramArray = [];
-if (param !== null) {
-    paramArray = param.split(',');
-}
-
+paramArray = param.split(",");
 if (paramArray.length !== 3) {
-    let widget = await createMsgWidget("Invalid Parameters");
-    Script.setWidget(widget);
-    Script.complete();
-    return;
+	let widget = await createMsgWidget("Invalid Parameter");
+	Script.setWidget(widget);
+	Script.complete();
 }
 
 let loc_label = paramArray[0];
@@ -27,146 +23,136 @@ let busStopId = paramArray[1];
 let service = paramArray[2];
 
 let busInfo = await getBusTimings(busStopId, service);
-if (busInfo !== null) { return; }
-
 let { arrivalTimes, busDeckTypes } = busInfo;
 
 console.log(arrivalTimes);
 console.log(busDeckTypes);
 
-let widget = null;
-try {
-    widget = await createWidget(loc_label, service, arrivalTimes, busDeckTypes);
-} catch (e) {
-    console.log(e);
-    widget = await createMsgWidget("Unknown");
-}
-
+let widget = await createWidget(loc_label, service, arrivalTimes, busDeckTypes);
 Script.setWidget(widget);
 Script.complete();
 
 async function getBusTimings(busStopID, service) {
-    let api_key = ""; // API key for the bus API
-    if (api_key.length === 0) {
-        let widget = await createMsgWidget("No API Key");
-        Script.setWidget(widget);
-        Script.complete();
-        return;
-    }
+	let api_key = ""; // API key for the bus API
 
-    let params = `BusStopCode=${busStopID}&ServiceNo=${service}`;
-    let url = `http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?${params}`;
-    let headers = {
-        "accept": "application/json",
-        "AccountKey": api_key
-    };
-    
-    let req = new Request(url);
-    req.headers = headers;
+	let params = `BusStopCode=${busStopID}&ServiceNo=${service}`;
+	let url = `http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?${params}`;
+	let headers = {
+		accept: "application/json",
+		AccountKey: api_key,
+	};
 
-    data = await req.loadJSON();
+	let req = new Request(url);
+	req.headers = headers;
 
-    if (!data || !data.Services || data.Services.length === 0) {
-        let widget = await createMsgWidget("API Request Failed");
-        Script.setWidget(widget);
-        Script.complete();
-        return;
-    }
+	data = await req.loadJSON();
 
-    let services = data.Services;
-    let firstService = services[0];
-    let arrivalTimes = [];
-    let busDeckTypes = [];
+	let services = data.Services;
+	let firstService = services[0];
+	let arrivalTimes = [];
+	let busDeckTypes = [];
 
-    for (let i = 1; i <= 3; i++) {
-        let nextBus = firstService[`NextBus${i === 1 ? '' : i}`];
-        let estimatedArrival = nextBus.EstimatedArrival || '-';
-        let busDeckType = nextBus.Type || '-';
+	for (let i = 1; i <= 3; i++) {
+		let nextBus =
+			firstService === undefined
+				? null
+				: firstService[`NextBus${i === 1 ? "" : i}`];
+		let estimatedArrival = nextBus === null ? null : nextBus.EstimatedArrival;
+		let busDeckType = nextBus === null ? "" : nextBus.Type;
 
-        arrivalTimes.push(estimatedArrival);
-        busDeckTypes.push(busDeckType);
-    }
+		arrivalTimes.push(estimatedArrival);
+		busDeckTypes.push(busDeckType);
+	}
 
-    return { arrivalTimes, busDeckTypes };
+	return { arrivalTimes, busDeckTypes };
 }
 
 async function createWidget(loc, service, arrivalTimes, busDeckTypes) {
-    let widget = new ListWidget();
-    widget.setPadding(2, 2, 2, 2);
+	let widget = new ListWidget();
+	widget.setPadding(2, 2, 2, 2);
 
-    let nextRefresh = Date.now() + 1000*30; // add 30 second to now
+	let nextRefresh = Date.now() + 1000 * 30; // add 30 second to now
 	widget.refreshAfterDate = new Date(nextRefresh);
 
-    // add bus service and arrival time
-    let location = widget.addText(loc);
-    location.font = Font.boldSystemFont(6);
-    location.centerAlignText();
+	// add bus service and arrival time
+	let location = widget.addText(loc);
+	location.font = Font.boldSystemFont(6);
+	location.centerAlignText();
 
-    let busService = widget.addText(service);
-    if (service.length > 2) {
-        busService.font = Font.boldSystemFont(22 - 4 * (service.length - 2));
-    } else {
-        busService.font = Font.boldSystemFont(22);
-    }
-    busService.centerAlignText();
+	let busService = widget.addText(service);
+	if (service.length > 2) {
+		busService.font = Font.boldSystemFont(22 - 4 * (service.length - 2));
+	} else {
+		busService.font = Font.boldSystemFont(22);
+	}
+	busService.centerAlignText();
 
-    let title = widget.addText("ARRIVAL - MIN");
-    title.font = Font.boldSystemFont(5);
-    title.centerAlignText();
+	let title = widget.addText("ARRIVAL - MIN");
+	title.font = Font.boldSystemFont(5);
+	title.centerAlignText();
 
-    console.log(arrivalTimes)
-    let arrivalTimeText = arrivalTimes.map(time => {
-        if (time === '-') {
-            return '--';
-        }
-        let arrivalTime = new Date(time);
-        let now = new Date();
-        let diff = Math.floor((arrivalTime - now) / 1000 / 60);
-        if (diff === 0) {
-            return 'A';
-        } else if (diff < 0) {
-            return 'L';
-        } else {
-            return diff.toString().padStart(2, '0');
-        }
-    }).join(" ");
+	console.log(arrivalTimes);
+	let arrivalTimeText = arrivalTimes
+		.map((time) => {
+			if (typeof time !== "number") {
+				return "";
+			}
 
-    let arrivalTime = widget.addText(arrivalTimeText);
-    
-    arrivalTime.font = Font.boldSystemFont(8);
-    arrivalTime.centerAlignText();
-    
-    let busDeckSizeText = busDeckTypes.map(type => {
-        if (type === 'SD') {
-            return 'S';
-        } else if (type === 'DD') {
-            return 'D';
-        } else if (type === 'BD') {
-            return 'B'
-        } else {
-            return '-';
-        }
-    }).join('  ');
+			let arrivalTime = new Date(time);
+			let now = new Date();
+			let diff = Math.floor((arrivalTime - now) / 1000 / 60);
+			if (diff === 0) {
+				return "A";
+			} else if (diff < 0) {
+				return "L";
+			} else {
+				return diff.toString().padStart(2, "0");
+			}
+		})
+		.join(" ");
 
-    let busDeckSize = widget.addText(busDeckSizeText);
-    busDeckSize.font = Font.boldSystemFont(10);
-    busDeckSize.centerAlignText();
+	const NO_SERVICE = arrivalTimeText.trim().length === 0;
+	if (NO_SERVICE) {
+		arrivalTimeText = "NO SERVICE";
+	}
+	let arrivalTime = widget.addText(arrivalTimeText);
 
-    widget.addSpacer();
-    return widget;
+	if (NO_SERVICE) {
+		arrivalTime.font = Font.boldSystemFont(6);
+	} else {
+		arrivalTime.font = Font.boldSystemFont(8);
+	}
+	arrivalTime.centerAlignText();
+
+	let busDeckSizeText = busDeckTypes
+		.map((type) => {
+			if (type === "SD") {
+				return "S";
+			} else if (type === "DD") {
+				return "D";
+			} else {
+				return "";
+			}
+		})
+		.join("  ");
+
+	let busDeckSize = widget.addText(busDeckSizeText);
+	busDeckSize.font = Font.boldSystemFont(10);
+	busDeckSize.centerAlignText();
+
+	widget.addSpacer();
+	return widget;
 }
 
 async function createMsgWidget(msg) {
-    let widget = new ListWidget();
-    widget.setPadding(2, 2, 2, 2);
+	let widget = new ListWidget();
+	widget.setPadding(2, 2, 2, 2);
 
-    let title = widget.addText("ERROR");
-    title.font = Font.boldSystemFont(8);
-    title.centerAlignText();
+	let title = widget.addText("ERROR");
+	title.font = Font.boldSystemFont(8);
+	title.centerAlignText();
 
-    let msgTB = widget.addText(msg);
-    msgTB.font = Font.boldSystemFont(6);
-    msgTB.centerAlignText();
-
-    return widget;
+	let widMsg = widget.addText(msg);
+	widMsg.font = Font.boldSystemFont(6);
+	widMsg.centerAlignText();
 }
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 <img src="https://raw.githubusercontent.com/LZXCorp/iOSBusTimings/adc02139d2ae5f056e615bbf7cc5d3c01770ab4a/pv.jpg" width="140" style="border-radius: 12%"/>
 
-##  INFORMATION
+## INFORMATION
 
 An iOS Widget designed to show at a glance arrival timings of Buses directly on your lockscreen.
 This only works for Singapore Public Bus Transits.
@@ -12,7 +12,8 @@ This only works for Singapore Public Bus Transits.
 ## PATCHES
 
 As of the November 2023 Patch,
-- New ```B - Bendy``` Bus Type added, which previously did not exist.
+
+- New `B - Bendy` Bus Type added, which previously did not exist.
 - There is now error handling for parameters and API errors.
 - Instead of a long string of text indicating 'Undefined' for arrival times, or out-right stop working whens services end, it will now show a 'SERVICE ENDED' badge.
 
@@ -29,12 +30,12 @@ As of the November 2023 Patch,
 - Create a new Scriptable Script by clicking on the '+' icon on the top right.
 - Rename the script to your liking.
 - Copy & paste [BusTimingsWidget_Public.js](https://raw.githubusercontent.com/LZXCorp/iOSBusTimings/main/BusTimingsWidget_Public.js) code into the script.
-- In the script, replace the ```api_key``` with the key you got from Land Transport DataMall.
+- In the script, replace the `api_key` with the key you got from Land Transport DataMall.
 
 - Now, on your lockscreen, hold down on your screen until you are in edit mode.
 - Click on the widgets portion of the lockscreen and put in a new Scriptable Widget.
 - Select your Bus Timings script and input the parameters.
-- Parameter Format: Parameter Format: ```<label>,<bus stop id>,<bus service>``` (Note: Do **not** leave any spacing between the parameters)
+- Parameter Format: Parameter Format: `<label>,<bus stop id>,<bus service>` (Note: Do **not** leave any spacing between the parameters)
 
 ## ACRYONYMS
 
@@ -52,11 +53,13 @@ As of the November 2023 Patch,
 
 \
 **BUS LOAD (TO BE ADDED)**
+
 - **L** - Low Load (Seats Available)
 - **M** - Medium Load (Standing Available)
 - **H** - High Load (Limited Standing)
 
 ## ⚠️ LIMITATIONS ⚠️
+
 - The widget is only designed for iPhone devices.
 - The text might be too small and unreadable for some users.
 - The updating rate of the widget ranges from 30 secs to 5 mins, making it unreliable.
@@ -64,17 +67,19 @@ As of the November 2023 Patch,
 
 ## WORKING ON
 
-From the November 2023 Patch,
-- [x] **BUG** - Error occurs when the service stops running, a message will be shown instead stating that service has ended.
-- [x] **QOL** - Shows an error message indicating which part of the parameter is in need of modifying.
+From the January 2024 Patch,
+
+- [x] **BUG** - Error still occuring when Bus services has terminated for the day
 
 \
 Still working on,
+
 - [ ] **LIMIT** - Label limitation of 11 characters.
 - [ ] **QOL** - Shows the last updated time of the widget.
 
 \
 Not-started-on features,
+
 - [ ] **FEATURE** - Ability through parameters to change what information is shown at the bottom. (I.e. instead of showing type of bus, it will show the bus load.)
 
 \
